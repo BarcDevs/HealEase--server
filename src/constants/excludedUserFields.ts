8e227ede8577902ac6a83e29aa616b66a8647daf
Bar Cohen <sscohen99@gmail.com>
2024-03-26 01:27:44 +0200

    [misc] added excludedUserFields

---
+++ src/constants/excludedUserFields.ts
@@ -0,0 +1,11 @@
+import { ServerUserType } from '../types/data/UserType'
+
+export const excludedUserFields: (keyof ServerUserType)[] = [
+    'password',
+    'resetPasswordOTP',
+    'resetPasswordExpiration',
+    'password_updated_at',
+    'created_at',
+    'active',
+    'deleted_at',
+]
