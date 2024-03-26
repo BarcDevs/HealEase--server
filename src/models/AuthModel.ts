345df24e25abcceb657e954331ee78219e5cde6f
Bar Cohen <sscohen99@gmail.com>
2024-03-27 01:46:41 +0200

    [rfc] removed catchModel function as it founds to cause multiple problems

--- src/models/AuthModel.ts
+++ src/models/AuthModel.ts
@@ -1,106 +1,83 @@
 import { NewUserType, ServerUserType } from '../types/data/UserType'
 import Prisma from '../utils/PrismaClient'
-// @ts-ignore
-import { catchModel } from '../utils/catch'
-// @ts-ignore
-import { ErrorPrefixes } from '../constants/errorPrefixes'
 
-const getUserById = catchModel(
-    async (id: string): Promise<ServerUserType | null> => {
-        const user = await Prisma.user.findUnique({
-            where: {
-                id,
-                active: true,
-            },
-        })
-
-        if (!user) return null
-
-        return user as ServerUserType
-    },
-    ErrorPrefixes.FETCH,
-)
-
-const getUserByEmail = catchModel(
-    async (email: string): Promise<ServerUserType | null> => {
-        const user = await Prisma.user.findUnique({
-            where: {
-                email,
-                active: true,
-            },
-        })
-
-        if (!user) return null
-
-        return user as ServerUserType
-    },
-    ErrorPrefixes.FETCH,
-)
-
-const createUser = catchModel(
-    (newUser: NewUserType): Promise<ServerUserType> =>
-        Prisma.user.create({
-            data: newUser,
-        }) as Promise<ServerUserType>,
-    ErrorPrefixes.REGISTRATION,
-)
-
-const updateUser = catchModel(
-    (
-        userId: string,
-        newUserData: Partial<NewUserType>,
-    ): Promise<ServerUserType> =>
-        Prisma.user.update({
-            where: {
-                id: userId,
-                active: true,
-            },
-            data: newUserData,
-        }) as Promise<ServerUserType>,
-    ErrorPrefixes.UPDATE,
-)
-
-export const setUserOTP = catchModel(
-    (
-        userId: string,
-        data: {
-            resetPasswordOTP: number | null
-            resetPasswordExpiration: Date | null
-            password_updated_at?: Date
+const getUserById = async (id: string): Promise<ServerUserType | null> => {
+    const user = await Prisma.user.findUnique({
+        where: {
+            id,
+            active: true,
         },
-    ): Promise<ServerUserType> =>
-        Prisma.user.update({
-            where: {
-                id: userId,
-                active: true,
-            },
-            data,
-        }) as Promise<ServerUserType>,
-    ErrorPrefixes.UPDATE,
-)
+    })
 
-const disableUser = catchModel(
-    (id: string): Promise<ServerUserType> =>
-        Prisma.user.update({
-            where: {
-                id,
-            },
-            data: {
-                active: false,
-            },
-        }) as Promise<ServerUserType>,
-    ErrorPrefixes.DELETE,
-)
+    if (!user) return null
 
-const deleteUser = catchModel(
-    (id: string): Promise<ServerUserType> =>
-        Prisma.user.delete({
-            where: {
-                id,
-            },
-        }) as Promise<ServerUserType>,
-    ErrorPrefixes.DELETE,
-)
+    return user as ServerUserType
+}
+
+const getUserByEmail = async (
+    email: string,
+): Promise<ServerUserType | null> => {
+    const user = await Prisma.user.findUnique({
+        where: {
+            email,
+            active: true,
+        },
+    })
+
+    if (!user) return null
+
+    return user as ServerUserType
+}
+
+const createUser = (newUser: NewUserType): Promise<ServerUserType> =>
+    Prisma.user.create({
+        data: newUser,
+    }) as Promise<ServerUserType>
+
+const updateUser = (
+    userId: string,
+    newUserData: Partial<NewUserType>,
+): Promise<ServerUserType> =>
+    Prisma.user.update({
+        where: {
+            id: userId,
+            active: true,
+        },
+        data: newUserData,
+    }) as Promise<ServerUserType>
+
+export const setUserOTP = (
+    userId: string,
+    data: {
+        resetPasswordOTP: number | null
+        resetPasswordExpiration: Date | null
+        password_updated_at?: Date
+    },
+): Promise<ServerUserType> =>
+    Prisma.user.update({
+        where: {
+            id: userId,
+            active: true,
+        },
+        data,
+    }) as Promise<ServerUserType>
+
+const disableUser = (id: string): Promise<ServerUserType> =>
+    Prisma.user.update({
+        where: {
+            id,
+        },
+        data: {
+            active: false,
+        },
+    }) as Promise<ServerUserType>
+
+const deleteUser = (id: string): Promise<ServerUserType> =>
+    Prisma.user.delete({
+        where: {
+            id,
+        },
+    }) as Promise<ServerUserType>
 
 export {
     getUserById,
--- src/utils/catch.ts
+++ src/utils/catch.ts
@@ -1,9 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const catchModel = (fn: (...args: any) => any, msgPrefix = '') => {
-    try {
-        return fn()
-    } catch (error) {
-        const err = error as Error
-        throw new Error(`${msgPrefix}${err.message}`)
-    }
-}
+// export const catchModel = <T>(fn: (...args: any) => any, msgPrefix = '') : Promise<T> => {
+//     try {
+//         return fn()
+//     } catch (error) {
+//         const err = error as Error
+//         throw new Error(`${msgPrefix}${err.message}`)
+//     }
+// }
