b0abea77e561a45332b8bb3a1512251690a27e49
Bar Cohen <sscohen99@gmail.com>
2024-03-28 02:28:44 +0200

    [feat] sanitation middleware now will set req.body to null if empty to ease req.body empty checks

--- src/middlewares/sanitaization.ts
+++ src/middlewares/sanitaization.ts
@@ -7,14 +7,21 @@ const { JSDOM } = jsdom
 const { window } = new JSDOM('')
 const DOMPurify = createDOMPurify(window)
 
+const isBodyEmpty = (req: Request) => Object.keys(req.body).length === 0
+
 export const sanitizeData = (
     req: Request,
     _res: Response,
-    next: NextFunction,
+    next: NextFunction
 ) => {
+    if (isBodyEmpty(req)) {
+        req.body = null
+        return next()
+    }
+
     Object.keys(req.body).forEach((key) => {
         req.body[key] = DOMPurify.sanitize(req.body[key])
     })
 
-    next()
+    return next()
 }
