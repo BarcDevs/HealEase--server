7b86cdeb57f02e3cfff6fabfa4fbd5407ba2275e
Bar Cohen <sscohen99@gmail.com>
2024-03-28 19:38:28 +0200

    [rfc] updated prisma client settings

--- src/utils/PrismaClient.ts
+++ src/utils/PrismaClient.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@prisma/client'
+import { env } from '../../config'
 
 let client: PrismaClient
 
 export const getPrismaClient = (): PrismaClient => {
-    if (!client) client = new PrismaClient()
+    if (!client)
+        client = new PrismaClient({
+            errorFormat: 'minimal',
+            log:
+                env === 'development'
+                    ? ['query', 'info', 'warn', 'error']
+                    : undefined
+        })
 
     return client
 }
