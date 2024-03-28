d2c3ba05281f11f61a532487ae48386501121906
Bar Cohen <sscohen99@gmail.com>
2024-03-28 02:30:21 +0200

    [feat] added form route controllers

---
+++ src/controllers/FormController.ts
@@ -0,0 +1,136 @@
+import { NextFunction, Request, Response } from 'express'
+import { postQuerySchema } from '../schemas/forum/postQuerySchema'
+import { ValidationError } from '../errors/ValidationError'
+import * as forumService from '../services/forumService'
+import { PostType } from '../types/data/PostType'
+import { successResponse } from '../responses/success'
+import { newPostSchema } from '../schemas/forum/newPostSchema'
+import { AuthError } from '../errors/AuthError'
+import { HttpStatusCodes } from '../constants/httpStatusCodes'
+import { updatePostSchema } from '../schemas/forum/updatePostSchema'
+import { errorFactory } from '../errors/factory'
+import { tagQuerySchema } from '../schemas/forum/tagQuerySchema'
+import { TagType } from '../types/data/TagType'
+
+export const getPosts = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const validatedQuery =
+        req.body &&
+        ValidationError.catchValidationErrors(
+            postQuerySchema.validate(req.body)
+        )
+
+    const data = (await forumService.getPosts(validatedQuery)) as
+        | PostType[]
+        | null
+
+    if (!data)
+        // todo throw next() error with generic error class
+        return res.status(404).json({ message: 'posts not found' })
+
+    return successResponse<PostType[]>(res, data)
+}
+
+export const createPost = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const validatedData = ValidationError.catchValidationErrors(
+        newPostSchema.validate({
+            ...req.body,
+            tags: req.body.tags.split(',')
+        })
+    )
+    const { userId } = req.locals || {}
+
+    if (!userId)
+        throw new AuthError(
+            'Unauthorized! please login first!',
+            undefined,
+            'Unauthorized',
+            HttpStatusCodes.UNAUTHORIZED
+        ) // todo AuthError builder
+
+    const data = await forumService.createPost({
+        ...validatedData,
+        authorId: userId
+    })
+    return successResponse<PostType>(res, data)
+}
+
+export const getPost = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const data = (await forumService.getPosts(
+        undefined,
+        req.params.postId
+    )) as PostType | null
+
+    if (!data)
+        // todo throw next() error with generic error class
+        return res.status(404).json({ message: 'post not found' })
+
+    return successResponse<PostType>(res, data)
+}
+
+export const updatePost = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const validatedData = ValidationError.catchValidationErrors(
+        updatePostSchema.validate(req.body)
+    )
+    const { postId } = req.params
+    const { userId } = req.locals || {}
+
+    if (!userId) throw errorFactory.auth.unauthorized()
+
+    if (!(await forumService.validateOwner('post', postId, userId)))
+        throw errorFactory.auth.unauthorized(
+            'you are not the author of this post!'
+        )
+
+    const data = await forumService.updatePost(postId, validatedData)
+    return successResponse<PostType>(res, data)
+}
+
+export const createReply = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {}
+
+export const getReply = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {}
+
+export const updateReply = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {}
+
+export const getTags = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    const validatedQuery =
+        req.query &&
+        ValidationError.catchValidationErrors(
+            tagQuerySchema.validate(req.query)
+        )
+
+    const data = await forumService.getTags(validatedQuery)
+
+    return successResponse<TagType[]>(res, data)
+}
