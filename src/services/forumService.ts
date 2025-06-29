e18862f76b4f0fe9abb3a1f8ddaa4581f6a5d1b7
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-06-29 21:15:41 +0300

    style(service): style forum service to more clean code, also ordered in regions

--- src/services/forumService.ts
+++ src/services/forumService.ts
@@ -1,11 +1,15 @@
-import { PostQuery, TagQuery } from '../types/query'
-import * as forumModel from '../models/ForumModel'
-import { getTagsByPostId } from '../models/ForumModel'
-import { NewPostType, UpdatePostType } from '../types/data/PostType'
 import { ErrorFactory, errorFactory } from '../errors/factory'
-import { NewReplyType, UpdateReplyType } from '../types/data/ReplyType'
+import { getTagsByPostId } from '../models/ForumModel'
+
+import * as forumModel from '../models/ForumModel'
+
+
+import type { NewPostType, UpdatePostType } from '../types/data/PostType'
+import type { NewReplyType, UpdateReplyType } from '../types/data/ReplyType'
+import type { PostQuery, TagQuery } from '../types/query'
 import { capitalizeText } from '../utils/capitalizeText'
 
+// region Validation
 export const validateOwner = async (
     schema: 'post' | 'reply',
     postId: string,
@@ -20,56 +24,103 @@ export const validateOwner = async (
             ? await forumModel.getPost(postId)
             : await forumModel.getReply(postId, replyId!)
 
-    if (!data) throw errorFactory.generic.notFound(capitalizeText(schema))
+    if (!data)
+        throw errorFactory.generic.notFound(capitalizeText(schema))
 
     if (data.authorId !== userId)
         throw errorFactory.auth.unauthorized(
             `you are not the author of this ${schema}!`
         )
 }
+// endregion
 
-export const getPosts = async (query?: PostQuery, id?: string) => {
-    if (id) return forumModel.getPost(id)
+// region Posts
+export const getPosts = async (
+    query?: PostQuery,
+    id?: string
+) => {
+    if (id)
+        return forumModel.getPost(id)
+
     return forumModel.getPosts(query)
 }
 
-export const getPostsCount = async (query?: PostQuery) =>
+export const getPostsCount = async (
+    query?: PostQuery
+) =>
     forumModel.getPostsCount(query)
 
-export const createPost = async (post: NewPostType) =>
+export const createPost = async (
+    post: NewPostType
+) =>
     forumModel.createPost(post)
 
-export const updatePost = async (id: string, post: UpdatePostType) => {
-    const prevTags = post.tags ? await getTagsByPostId(id) : undefined
-    const removeTags = prevTags?.filter((tag) => !post.tags?.includes(tag.name))
+export const updatePost = async (
+    id: string,
+    post: UpdatePostType
+) => {
+    const prevTags = post.tags ?
+        await getTagsByPostId(id) :
+        undefined
+    const removeTags =
+        prevTags?.filter(
+            (tag) => !post.tags?.includes(tag.name)
+        )
 
     return forumModel.updatePost(id, post, removeTags)
 }
 
-export const deletePost = async (id: string) => forumModel.deletePost(id)
+export const deletePost = async (
+    id: string
+) =>
+    forumModel.deletePost(id)
+// endregion
 
+// region Tags
 export const getTags = async (
     options: TagQuery | { filter: 'id'; id: string }
 ) =>
     options.filter === 'id'
         ? forumModel.getTagsByPostId(options.id)
         : options.filter === 'popular'
-          ? forumModel.getPopularTags(options.limit)
-          : forumModel.getTags(options.search, options.limit, options.page)
+            ? forumModel.getPopularTags(options.limit)
+            : forumModel.getTags(
+                options.search,
+                options.limit,
+                options.page
+            )
 
-export const getTag = async (id: string) => forumModel.getTag(id)
+export const getTag = async (
+    id: string
+) =>
+    forumModel.getTag(id)
 
-export const createReply = async (reply: NewReplyType) =>
+export const createReply = async (
+    reply: NewReplyType
+) =>
     forumModel.createReply(reply)
+// endregion
 
-export const getReplies = async (postId: string) =>
+// region Replies
+export const getReplies = async (
+    postId: string
+) =>
     forumModel.getReplies(postId)
 
 export const updateReply = async (
     replyId: string,
     postId: string,
     reply: UpdateReplyType
-) => forumModel.updateReply(replyId, postId, reply)
+) =>
+    forumModel.updateReply(
+        replyId,
+        postId,
+        reply
+    )
 
-export const deleteReply = async (replyId: string, postId: string) =>
+export const deleteReply = async (
+    replyId: string,
+    postId: string
+) =>
     forumModel.deleteReply(replyId, postId)
+// endregion
