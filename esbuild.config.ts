c4fa19ac20ce791ff9c2337b252825a6a85f4126
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-09-05 10:43:10 +0300

    fix: fix esbuild config

--- esbuild.config.ts
+++ esbuild.config.ts
@@ -1,28 +1,17 @@
 import { build } from 'esbuild'
 
-async function main () {
-    try {
-        await build({
-            entryPoints: ['src/app.ts'],
-            bundle: true,
-            platform: 'node',
-            outfile: 'dist/server.js',
-            sourcemap: true,
-            minify: false,
-            external: [
-                'aws-sdk',
-                'mock-aws-s3',
-                'nock',
-                'jsdom',
-                '@prisma/client',
-                'prisma'
-            ]
-        })
-        console.log('Build completed successfully!')
-    } catch ( err ) {
-        console.error('Build failed:', err)
-        process.exit(1)
-    }
-}
-
-main()
+build({
+    entryPoints: ['src/app.ts'],
+    bundle: true,
+    platform: 'node',
+    target: 'node20',
+    outfile: 'dist/server.js',
+    external: [
+        'aws-sdk',
+        'mock-aws-s3',
+        'nock',
+        'jsdom',
+        '@prisma/client',
+        'prisma'
+    ]
+}).catch(() => process.exit(1))
